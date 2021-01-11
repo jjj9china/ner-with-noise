d0ff13bf66c2a916e6c4d91c5f3f62849d0ca533
jjj9china <32569700+jjj9china@users.noreply.github.com>
2021-01-11 16:18:43 +0800

    Create student.py

---
+++ src/mentornet/student.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: jjj
+# @Date:   2021-01-11
+
+from baseline.bilstm import BiLstm
+from baseline.bilstm_crf import BiLstmCrf
+from partialcrf.bilstm_partialcrf import BiLstmPartialCrf
