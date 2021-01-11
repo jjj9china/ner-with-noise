3056a937d878585cedc3d08ef7f42684ed041f3f
jjj9china <32569700+jjj9china@users.noreply.github.com>
2021-01-11 16:17:52 +0800

    Create config.py

---
+++ src/mentornet/config.py
@@ -0,0 +1,16 @@
+
+# student paras
+student_batch_size = 64
+embedding_size = 100
+hidden_size = 128
+
+epoches = 10
+early_stop = 3
+
+print_step = 10
+best_val_loss = 1e18
+lr = 0.001
+
+# mentor paras
+predict_threshold = 0.3
+mentor_batch_size = 16
