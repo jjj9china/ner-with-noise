3fdfd3432c9abf36a632b8d3d290eb73a6217b5f
jjj9china <32569700+jjj9china@users.noreply.github.com>
2021-01-16 20:58:52 +0800

    Update mentor.py

--- src/mentornet/mentor.py
+++ src/mentornet/mentor.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 # @Author: jjj
-# @Date:   2021-01-13
+# @Date:   2021-01-16
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
-
-from preprocess import config as DataConfig
 
 
 OUTPUT_DIM = 1  # 二分类问题，判断当前token是否为噪声
@@ -37,17 +35,15 @@ class MLP(nn.Module):
         x = self.fc2(x)   # [B, L, 1]
         return x
 
-    def cal_loss(self, logits, targets, tag2id, pos_weight):
+    def cal_loss(self, logits, targets, mask, pos_weight):
         """计算损失
 
         Args:
             logits: [B, L, out_size]
             targets: [B, L]
-            tag2id: dict
+            mask: [B, L]
             pos_weight: positive weight. Note that default positive class is normal one.
         """
-        PAD = tag2id.get(DataConfig.PAD_TOKEN)
-        mask = (targets != PAD)  # [B, L]
         targets = targets[mask]  # get real target
         logits = logits.squeeze(2).masked_select(mask).contiguous().view(-1)
 
@@ -89,27 +85,21 @@ class BiLstm(nn.Module):
 
         return scores
 
-    def test(self, sents_tensor, lengths, _):
-        """第三个参数不会用到，加它是为了与BiLSTM_CRF保持同样的接口
-        """
+    def test(self, sents_tensor, lengths):
         logits = self.forward(sents_tensor, lengths)  # [B, L, out_size]
         _, batch_tagids = torch.max(logits, dim=2)
 
         return batch_tagids
 
-    def cal_loss(self, logits, targets, tag2id, pos_weight):
+    def cal_loss(self, logits, targets, mask, pos_weight):
         """计算 BiLSTM 损失
 
         Args:
             logits: [B, L, out_size]
             targets: [B, L]
-            tag2id: dict,
+            mask: : [B, L]
             pos_weight: positive weight. Note that default positive class is normal one.
         """
-        PAD = tag2id.get(DataConfig.PAD_TOKEN)
-        assert PAD is not None
-
-        mask = (targets != PAD)  # [B, L]
         targets = targets[mask]  # get real target
         logits = logits.squeeze(2).masked_select(mask).contiguous().view(-1)
 
